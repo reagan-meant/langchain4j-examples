bf824c3f037051519081275eea278af70aa0c5d2
reagan-meant <reaganmeant@gmail.com>
2025-02-18 18:39:45 +0300

    Connect to MYSQL for analysis

--- rag-examples/src/main/java/_3_advanced/_10_Advanced_RAG_SQL_Database_Retreiver_Example.java
+++ rag-examples/src/main/java/_3_advanced/_10_Advanced_RAG_SQL_Database_Retreiver_Example.java
@@ -1,28 +1,32 @@
 package _3_advanced;
 
-import _2_naive.Naive_RAG_Example;
-import dev.langchain4j.experimental.rag.content.retriever.sql.SqlDatabaseContentRetriever;
-import dev.langchain4j.memory.chat.MessageWindowChatMemory;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.openai.OpenAiChatModel;
-import dev.langchain4j.rag.content.retriever.ContentRetriever;
-import dev.langchain4j.service.AiServices;
-import org.h2.jdbcx.JdbcDataSource;
-import shared.Assistant;
-import static dev.langchain4j.internal.Utils.getOrDefault;
-
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.sql.DataSource;
+
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import _2_naive.Naive_RAG_Example;
+import dev.langchain4j.experimental.rag.content.retriever.sql.SqlDatabaseContentRetriever;
+import static dev.langchain4j.internal.Utils.getOrDefault;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
-import static shared.Utils.*;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.service.AiServices;
+import shared.Assistant;
+import static shared.Utils.startConversationWith;
+import static shared.Utils.toPath;
 
 public class _10_Advanced_RAG_SQL_Database_Retreiver_Example {
-    static String GEMINI_API_KEY = getOrDefault(System.getenv("GEMINI_API_KEY"), "");
+    static String GEMINI_API_KEY = getOrDefault(System.getenv("GEMINI_API_KEY"), "demoKey");
+    static String DB_URL = getOrDefault(System.getenv("DB_URL"), "jdbc:mariadb://localhost:3306/openmrs");
+    static String DB_USER = getOrDefault(System.getenv("DB_USER"), "root");
+    static String DB_PASSWORD = getOrDefault(System.getenv("DB_PASSWORD"), "password");
 
 
     /**
@@ -61,6 +65,7 @@ public class _10_Advanced_RAG_SQL_Database_Retreiver_Example {
 
         ContentRetriever contentRetriever = SqlDatabaseContentRetriever.builder()
                 .dataSource(dataSource)
+                .sqlDialect("MySQL")
                 .chatLanguageModel(chatLanguageModel)
                 .build();
 
@@ -73,16 +78,22 @@ public class _10_Advanced_RAG_SQL_Database_Retreiver_Example {
 
     private static DataSource createDataSource() {
 
-        JdbcDataSource dataSource = new JdbcDataSource();
-        dataSource.setURL("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
-        dataSource.setUser("sa");
-        dataSource.setPassword("sa");
+        DataSource dataSource = setupDataSource();
 
-        String createTablesScript = read("sql/create_tables.sql");
-        execute(createTablesScript, dataSource);
+        return dataSource;
+    }
 
-        String prefillTablesScript = read("sql/prefill_tables.sql");
-        execute(prefillTablesScript, dataSource);
+    private static DataSource setupDataSource() {
+        MariaDbDataSource dataSource = new MariaDbDataSource();
+
+        try {
+            dataSource.setUrl(DB_URL);
+            dataSource.setUser(DB_USER);
+            dataSource.setPassword(DB_PASSWORD);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return dataSource;
     }
